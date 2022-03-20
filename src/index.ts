e33d8df3217343f3ffa621f529219fbcea4aa16d
wangwei1 <wangwei1@hualala.com>
2022-03-20 13:18:56 +0800

    feat: 添加ErrorBoundary

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,7 @@ export {
 } from "./sdk";
 export { MiniappOptions } from "./backend";
 export { MiniappClient, ReportDialogOptions } from "./client";
+// export { ErrorBoundary, withErrorBoundary } from "./errorboundary";
 
 import * as Integrations from "./integrations/index";
 import * as Transports from "./transports/index";
